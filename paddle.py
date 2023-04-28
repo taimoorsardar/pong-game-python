c4686ebe62f5597f731f9ef8c147b70f45db4b65
taimoorsardar <113387247+taimoorsardar@users.noreply.github.com>
2023-04-28 14:36:12 +0500

    Add game files.
    All game files were added to be shown in the root directory.

---
+++ paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 30
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 30
+        self.goto(self.xcor(), new_y)
+
+    def reset_r_paddle(self):
+        self.goto((350,0))
+
+
+    def reset_l_paddle(self):
+        self.goto(-350,0)
+
